b4337b29245db90611e050cf01a0cd07ce364d11
Mikhail Volkov <55005467+readiv@users.noreply.github.com>
2021-09-29 16:25:28 +0200

    readme + sum_of_digits

--- hormiga.cpp
+++ hormiga.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <conio.h>
+#include <cmath>
+
+#define n_max 25
+#define init_x 1000
+#define init_y 1000
 
 using namespace std;
 
+int  sum_of_digits(int digits) {  // Возвращает сумму чисел в числе
+	int result = 0;
+	digits = abs(digits); //  Отрицательные числа
+	while (digits!=0) {
+		result += digits % 10;
+		digits =  digits / 10;
+    }
+	return result;
+}
 int main ()
 {
-    cout << "Hello world!!!\n";
-    _getch();
+	cout << sum_of_digits(123456789);
+	_getch();
     return 0;
 }
